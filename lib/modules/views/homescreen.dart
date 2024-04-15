28e90b43bfc999667fe825275940283a2ec1bd46
AnandUxd <anand.yadav@uxdlabtech.com>
2024-04-15 05:59:37 -0700

    third commit

--- lib/modules/views/homescreen.dart
+++ lib/modules/views/homescreen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
+
 class Homepage extends StatefulWidget {
-  const Homepage({super.key});
+  const Homepage({Key? key}) : super(key: key);
 
   @override
   State<Homepage> createState() => _HomepageState();
@@ -10,45 +11,58 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   double _progress = 0;
-  late InAppWebViewController  inAppWebViewController;
+  late InAppWebViewController inAppWebViewController;
+
   @override
   Widget build(BuildContext context) {
-    // ignore: deprecated_member_use
-    return WillPopScope(
-      onWillPop: ()async{
-        var isLastPage = await inAppWebViewController.canGoBack();
-        if(isLastPage){
-          inAppWebViewController.goBack();
-          return false;
-        }
-        return true;
-      },
-      child: SafeArea(
-        child: Scaffold(
-          body: Stack(
-            children: [
-              InAppWebView(
-                initialUrlRequest: URLRequest(
-                  url:WebUri("buyandsellfood.uxdlabtech.com")
-                ),
-                onWebViewCreated: (InAppWebViewController controller){
-                  inAppWebViewController = controller;
-                },
-                onProgressChanged: (InAppWebViewController controller , int progress){
-                  setState(() {
-                    _progress = progress / 100;
-                  });
-                },
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            InAppWebView(
+              initialUrlRequest: URLRequest(
+                url: WebUri("http://buyandsellfood.uxdlabtech.com"),
               ),
-              _progress < 1 ? Container(
-                child: LinearProgressIndicator(
-                  value: _progress,
-                ),
-              ):SizedBox()
-            ],
-          ),
+              onWebViewCreated: (InAppWebViewController controller) {
+                inAppWebViewController = controller;
+              },
+              onProgressChanged: (InAppWebViewController controller, int progress) {
+                setState(() {
+                  _progress = progress / 100;
+                });
+              },
+            ),
+            _progress < 1
+                ? Container(
+                    child: LinearProgressIndicator(
+                      value: _progress,
+                    ),
+                  )
+                : SizedBox(),
+          ],
         ),
       ),
     );
   }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _setupBackButtonListener(context);
+  // }
+
+//  void _setupBackButtonListener(BuildContext context) {
+//   inAppWebViewController.backButtonPressed.listen((_) {
+//     _handleBackButton(context);
+//   });
+// }
+
+//   Future<void> _handleBackButton(BuildContext context) async {
+//     if (await inAppWebViewController.canGoBack()) {
+//       inAppWebViewController.goBack();
+//     } else {
+//       // If web view can't go back, do nothing
+//     }
+//   }
+// }
 }
--- lib/modules/widgets/PageViewWidget.dart
+++ lib/modules/widgets/PageViewWidget.dart
@@ -1,8 +1,6 @@
-// ignore: file_names
 import 'package:buyandsellfood/modules/views/homescreen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 
 class PageViewWidget extends StatefulWidget {
   @override
@@ -14,104 +12,94 @@ class _PageViewWidgetState extends State<PageViewWidget> {
   int _currentPage = 0;
 
   @override
- Widget build(BuildContext context) {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.stretch,
-    children: <Widget>[
-      Expanded(
-        flex: 2,
-        child: PageView(
-          controller: _pageController,
-          onPageChanged: (int page) {
-            setState(() {
-              _currentPage = page;
-            });
-          },
-          children: <Widget>[
-            PageContent(
-              image: 'assets/image1.png',
-              heading: 'Discover Local Delights',
-              description: 'Explore a world of culinary wonders right in your neighborhood. From hidden gems to popular favorites, uncover the best flavors your city has to offer',
-            ),
-            PageContent(
-              image: 'assets/image3.png',
-              heading: 'Ordering Made Easy',
-              description: 'Streamline your ordering process with our user-friendly platform. From browsing menus to checkout, enjoy a seamless and hassle-free experience from start to finish',
-            ),
-            PageContent(
-              image: 'assets/image5.png',
-              heading: 'Customize Your Food Experience',
-              description: 'Tailor your dining journey to your taste buds whether it\'s dietary preferences or flavor cravings, personalize your food adventure with ease',
-            ),
-          ],
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 2,
+          child: PageView(
+            controller: _pageController,
+            onPageChanged: (int page) {
+              setState(() {
+                _currentPage = page;
+              });
+            },
+            children: <Widget>[
+              PageContent(
+                image: 'assets/image1.png',
+                heading: 'Discover Local Delights',
+                description:
+                    'Explore a world of culinary wonders right in your neighborhood. From hidden gems to popular favorites, uncover the best flavors your city has to offer',
+              ),
+              PageContent(
+                image: 'assets/image3.png',
+                heading: 'Ordering Made Easy',
+                description:
+                    'Streamline your ordering process with our user-friendly platform. From browsing menus to checkout, enjoy a seamless and hassle-free experience from start to finish',
+              ),
+              PageContent(
+                image: 'assets/image5.png',
+                heading: 'Customize Your Food Experience',
+                description:
+                    'Tailor your dining journey to your taste buds whether it\'s dietary preferences or flavor cravings, personalize your food adventure with ease',
+              ),
+            ],
+          ),
         ),
-      ),
-      SizedBox(height: 1.0),
-      Expanded(
-        flex: 1,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            PageIndicator(
-              currentPage: _currentPage,
-              pageCount: 3, // Number of pages
-            ),
-            SizedBox(height: 100.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                SizedBox(
-                  width: 160, // Set the width to half of the screen width
-                  height: 55.0, 
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                         context,
-                         MaterialPageRoute(builder: (context) => Homepage()),
-                        );
-                      
-                      // _pageController.previousPage(
-                      //   duration: Duration(milliseconds: 500),
-                      //   curve: Curves.ease,
-                     // );
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(13.0),
-                          side: BorderSide(color:  Color(0xFF13B251)), // Add border
+        SizedBox(height: 1.0),
+        Expanded(
+          flex: 1,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              PageIndicator(
+                currentPage: _currentPage,
+                pageCount: 3, // Number of pages
+              ),
+              SizedBox(height: 100.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  if (_currentPage != 2)
+                    SizedBox(
+                      width: 160, // Set the width to half of the screen width
+                      height: 55.0,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          _pageController.jumpToPage(2);
+                        },
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.white),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(13.0),
+                              side: BorderSide(
+                                  color: Color(0xFF13B251)), // Add border
+                            ),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            'Skip',
+                            style: TextStyle(
+                              fontFamily: GoogleFonts.montserrat().fontFamily,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                              color: Color(0xFF13B251),
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Text('Skip', style: TextStyle(
-                        fontFamily: GoogleFonts.montserrat().fontFamily,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 16,
-                         color: Color(0xFF13B251)),),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 160, // Set the width to half of the screen width
-                  height: 55.0, // Set the height as desired
-                  child: GestureDetector(
-                    onTap: () {
-                      if (_currentPage < 2) {
-                        _pageController.nextPage(
-                          duration: Duration(milliseconds: 500),
-                          curve: Curves.ease,
-                        );
-                      } else {
-                        Navigator.push(
-                         context,
-                         MaterialPageRoute(builder: (context) => Homepage()),
-                        );
-                        // Navigate to the next screen
-                      }
-                    },
+                  SizedBox(
+                    width: (_currentPage < 2)
+                        ? 160
+                        : 345, // Set the width based on the current page
+                    height: 55.0, // Set the height as desired
                     child: Container(
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(13.0),
@@ -124,34 +112,58 @@ class _PageViewWidgetState extends State<PageViewWidget> {
                           end: Alignment.centerRight,
                         ),
                       ),
-                      child: Center(
-                        child: Text(
-                          'Next',
-                          style: TextStyle(
-                            fontFamily: GoogleFonts.montserrat().fontFamily,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 16,
-                            color: Colors.white,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_currentPage < 2) {
+                            _pageController.nextPage(
+                              duration: Duration(milliseconds: 500),
+                              curve: Curves.ease,
+                            );
+                          } else {
+                            Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Homepage()),
+                                   (Route<dynamic> route) => false
+                            );
+                          }
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Colors.transparent),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(13.0),
+                              side: BorderSide(
+                                  color: Colors
+                                      .transparent), // No border for the button
+                            ),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            (_currentPage < 2) ? 'Next' : 'Get started',
+                            style: TextStyle(
+                              fontFamily: GoogleFonts.montserrat().fontFamily,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                              color: Colors
+                                  .white, // Text color for both "Next" and "Get started"
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
-    ],
-  );
-}
-
-
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
+      ],
+    );
   }
 }
 
@@ -160,47 +172,48 @@ class PageContent extends StatelessWidget {
   final String heading;
   final String description;
 
-  PageContent({required this.image, required this.heading, required this.description});
+  PageContent(
+      {required this.image, required this.heading, required this.description});
 
   @override
-Widget build(BuildContext context) {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-      AspectRatio(
-        aspectRatio: 18 / 18, // Set the aspect ratio as per your requirement
-        child: Image.asset(
-          image,
-          fit: BoxFit.cover,
-        ),
-      ),
-      SizedBox(height: 30.0),
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Text(
-          heading,
-          style: TextStyle(
-            fontFamily: GoogleFonts.montserrat().fontFamily,
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        AspectRatio(
+          aspectRatio: 18 / 18, // Set the aspect ratio as per your requirement
+          child: Image.asset(
+            image,
+            fit: BoxFit.cover,
           ),
         ),
-      ),
-      SizedBox(height: 8),
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Text(
-          description,
-          style: TextStyle(
-            fontFamily: GoogleFonts.montserrat().fontFamily,
-            fontSize: 16.0,
-            fontWeight: FontWeight.w400
+        SizedBox(height: 30.0),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Text(
+            heading,
+            style: TextStyle(
+              fontFamily: GoogleFonts.montserrat().fontFamily,
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-      ),
-    ],
-  );
-}
+        SizedBox(height: 8),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Text(
+            description,
+            style: TextStyle(
+              fontFamily: GoogleFonts.montserrat().fontFamily,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
 
 class PageIndicator extends StatelessWidget {
@@ -222,7 +235,9 @@ class PageIndicator extends StatelessWidget {
             height: 12.0,
             decoration: BoxDecoration(
               shape: BoxShape.circle,
-              color: (i == currentPage) ?  const Color(0xFF13B251) : const Color.fromARGB(255, 235, 231, 231),
+              color: (i == currentPage)
+                  ? const Color(0xFF13B251)
+                  : const Color.fromARGB(255, 235, 231, 231),
             ),
           ),
         ),
